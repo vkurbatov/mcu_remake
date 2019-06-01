2437f0c6304d581065706c72c7d0652f1a08d581
vkurbatov <kurbatovvv82@mail.ru>
2019-06-01 21:55:59 +0300

    au packetizer build

--- liblgcodec/codec/audio/aac/au_packet.cpp
+++ liblgcodec/codec/audio/aac/au_packet.cpp
@@ -1,5 +1,7 @@
 #include "au_packet.h"
 
+#include <cstring>
+
 namespace largo
 {
 
@@ -9,10 +11,96 @@ namespace codec
 namespace audio
 {
 
-au_packet::au_packet()
+AuPacketizer::AuPacketizer(const au_header_config_t &au_header_config)
+ : m_au_header_config(au_header_config)
 {
 
 }
+
+std::size_t AuPacketizer::PushData(const void *data, std::size_t size)
+{
+
+	std::size_t result = 0;
+
+	if (data != nullptr && size > 0)
+	{
+		m_packet_queue.emplace(static_cast<const std::uint8_t*>(data)
+						   , static_cast<const std::uint8_t*>(data) + size);
+		result = size;
+	}
+}
+
+std::size_t AuPacketizer::PopData(void *data, std::size_t size)
+{
+	std::size_t result = 0;
+
+	if (!m_packet_queue.empty())
+	{
+		const auto& packet = m_packet_queue.front();
+		result = packet.size();
+
+		if (data != nullptr)
+		{
+			result = std::min(result, size);
+			std::memcpy(data, packet.data(), result);
+			m_packet_queue.pop();
+		}
+	}
+
+	return result;
+}
+
+bool AuPacketizer::DropData()
+{
+	bool result = !m_packet_queue.empty();
+
+	if (result == true)
+	{
+		m_packet_queue.pop();
+	}
+
+	return result;
+}
+
+std::size_t AuPacketizer::PushPacket(void *packet, std::size_t size)
+{
+
+}
+
+std::size_t AuPacketizer::PopPacket(void *packet, std::size_t size)
+{
+
+}
+
+std::size_t AuPacketizer::Count() const
+{
+	return m_packet_queue.size();
+}
+
+void AuPacketizer::SetConfig(const au_header_config_t& au_header_config)
+{
+	m_au_header_config = au_header_config;
+}
+
+const au_header_config_t& AuPacketizer::GetConfig() const
+{
+	return m_au_header_config;
+}
+
+std::size_t AuPacketizer::Clear()
+{
+	auto result = Count();
+
+	while (DropData());
+
+	return result;
+}
+
+std::size_t AuPacketizer::get_need_packet_size(std::size_t available_size) const
+{
+
+}
+
 
 } // audio
 
--- liblgcodec/codec/audio/aac/au_packet.h
+++ liblgcodec/codec/audio/aac/au_packet.h
@@ -2,6 +2,8 @@
 #define AU_PACKET_H
 
 #include <cstdint>
+#include <queue>
+#include <vector>
 
 namespace largo
 {
@@ -12,7 +14,7 @@ namespace codec
 namespace audio
 {
 
-struct au_header
+struct au_header_t
 {
 	std::uint32_t	size;
 	std::uint32_t	index;
@@ -23,7 +25,7 @@ struct au_header
 	std::uint32_t	rap_flag;
 };
 
-struct au_header_config
+struct au_header_config_t
 {
 	std::uint32_t	au_size_length;
 	std::uint32_t	au_index_length;
@@ -32,12 +34,34 @@ struct au_header_config
 	std::uint32_t	au_dts_delta_length;
 };
 
-
-class AuPacket
+class AuPacketizer
 {
 
+	using packet_t = std::vector<std::uint8_t>;
+	using queue_t = std::queue<packet_t>;
+
+	au_header_config_t	m_au_header_config;
+	queue_t				m_packet_queue;
+
 public:
-	AuPacket();
+	AuPacketizer(const au_header_config_t& au_header_config);
+
+	std::size_t PushData(const void* data, std::size_t size);
+	std::size_t PopData(void* data = nullptr, std::size_t size = 0);
+	bool DropData();
+
+	std::size_t PushPacket(void* packet, std::size_t size);
+	std::size_t PopPacket(void* packet, std::size_t size);
+
+	std::size_t Count() const;
+	void SetConfig(const au_header_config_t& au_header_config);
+	const au_header_config_t& GetConfig() const;
+	std::size_t Clear();
+
+private:
+
+	std::size_t get_need_packet_size(std::size_t available_size) const;
+
 };
 
 } // audio
